5a73252b3de93d67ca6aaa6f59d7cfeaae66f98c
Alexey Seryogin <xellex.yung@yandex.ru>
2020-10-13 14:58:05 +0300

    init project

---
+++ YourNote/YourNote.BL/Validation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace YourNote.BL
+{
+    public static class Validation
+    {
+        public static bool IsLoginValid(string login)
+        {
+            Regex regex = new Regex(@"");
+
+            return regex.IsMatch(login);
+        }
+    }
+}
